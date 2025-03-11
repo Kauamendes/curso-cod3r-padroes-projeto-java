1539c4ab9e403082859610938a85eef8986af862
kaua.mendes@attornatus.com.br <kaua.mendes@attornatus.com.br>
2025-03-11 11:46:25 -0300

    Adicionando aulas sobre abstract factory

--- creational-patterns/src/main/java/br/com/cod3r/abstractfactory/app/service/Client.java
+++ creational-patterns/src/main/java/br/com/cod3r/abstractfactory/app/service/Client.java
@@ -1,8 +1,47 @@
 package br.com.cod3r.abstractfactory.app.service;
 
+import br.com.cod3r.abstractfactory.app.service.factory.EJBAbstractFactory;
+import br.com.cod3r.abstractfactory.app.service.factory.RestAbstractFactory;
+import br.com.cod3r.abstractfactory.app.service.factory.ServicesAbstractFactory;
+import br.com.cod3r.abstractfactory.app.service.services.CarService;
+import br.com.cod3r.abstractfactory.app.service.services.UserService;
+
 public class Client {
 
 	public static void main(String[] args) {
+		ServicesAbstractFactory ejbAbstractFactory = new EJBAbstractFactory();
+		ServicesAbstractFactory restAbstractService = new RestAbstractFactory();
 
+		UserService ejbUserService = ejbAbstractFactory.getUserService();
+		UserService restUserService = restAbstractService.getUserService();
+
+		System.out.println("### Salvando usuário com EJB");
+		ejbUserService.save("Usuario");
+
+		System.out.println("### Salvando usuário com rest");
+		restUserService.save("Usuario");
+
+		System.out.println("### Excluindo usuário com EJB");
+		ejbUserService.delete(1);
+
+		System.out.println("### Excluindo usuário com rest");
+		restUserService.delete(1);
+
+		System.out.println("\n------------------------------------------------------------\n");
+
+		CarService ejbCarService = ejbAbstractFactory.getCarService();
+		CarService restCarService = restAbstractService.getCarService();
+
+		System.out.println("### Salvando carro com EJB");
+		ejbCarService.save("Carro");
+
+		System.out.println("### Salvando carro com rest");
+		restCarService.save("Carro");
+
+		System.out.println("### Atualizando carro com EJB");
+		ejbCarService.update("Carro");
+
+		System.out.println("### Atualizando carro com rest");
+		restCarService.update("Carro");
 	}
 }
